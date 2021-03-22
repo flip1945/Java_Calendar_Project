f18c97142a00178ed62617fcf9df967221d0f40a
flip1945 <coghdus1016@naver.com>
2021-03-22 22:14:15 +0900

    Update Max Days of Month

--- src/honux/calendar/Calendar.java
+++ src/honux/calendar/Calendar.java
@@ -1,5 +1,7 @@
 package honux.calendar;
 
+import java.util.Scanner;
+
 public class Calendar {
 
 	public static void main(String[] args) {
@@ -9,7 +11,16 @@ public class Calendar {
 		System.out.println(" 8  9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");		
-		System.out.println("22 23 24 25 26 27 28");		
+		System.out.println("22 23 24 25 26 27 28");
+		
+		System.out.println("\n월을 입력해 주세요.");
+		
+		Scanner sc = new Scanner(System.in);
+		int month = sc.nextInt();
+		int[] days = {31,28,31,30,31,30,31,31,30,31,30,31};
+		sc.close();
+		
+		System.out.printf("%d월은 %d일까지 있습니다.", month, days[month - 1]);
 	}
 
 }
